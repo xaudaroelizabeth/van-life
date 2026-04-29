8f90d102f30656ddea66d9ffd6c880f541af1462
xaudaroelizabeth <133051325+xaudaroelizabeth@users.noreply.github.com>
2026-04-29 18:24:48 -0500

    remove node_modules

---
+++ api.js
@@ -0,0 +1,70 @@
+import { initializeApp } from "firebase/app";
+import {
+  getFirestore,
+  collection,
+  doc,
+  getDocs,
+  getDoc,
+  query,
+  where,
+} from "firebase/firestore/lite";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCCTf0rjYIyQlo4SyuIgIiD6jm9hNVw5WA",
+  authDomain: "vanlife-5b7e7.firebaseapp.com",
+  projectId: "vanlife-5b7e7",
+  storageBucket: "vanlife-5b7e7.firebasestorage.app",
+  messagingSenderId: "324706658579",
+  appId: "1:324706658579:web:3313051f4ae37b7aafd2e6",
+};
+
+const app = initializeApp(firebaseConfig);
+const db = getFirestore(app);
+
+const vansCollectionRef = collection(db, "vans");
+
+export async function getVans() {
+  const snapshot = await getDocs(vansCollectionRef);
+  const vans = snapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return vans;
+}
+
+export async function getVan(id) {
+  const docRef = doc(db, "vans", id);
+  const snapshot = await getDoc(docRef);
+  return {
+    ...snapshot.data(),
+    id: snapshot.id,
+  };
+}
+
+export async function getHostVans() {
+  const q = query(vansCollectionRef, where("hostId", "==", "123"));
+  const snapshot = await getDocs(q);
+  const vans = snapshot.docs.map((doc) => ({
+    ...doc.data(),
+    id: doc.id,
+  }));
+  return vans;
+}
+
+export async function loginUser(creds) {
+  const res = await fetch("/api/login", {
+    method: "post",
+    body: JSON.stringify(creds),
+  });
+  const data = await res.json();
+
+  if (!res.ok) {
+    throw {
+      message: data.message,
+      statusText: res.statusText,
+      status: res.status,
+    };
+  }
+
+  return data;
+}
---
+++ index.jsx
@@ -0,0 +1,65 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { BrowserRouter, Routes, Route, Link } from "react-router-dom";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
+import Vans from "./pages/Vans/Vans.jsx";
+import VanDetail from "./pages/Vans/VanDetail.jsx";
+import Login from "./pages/Login.jsx";
+import Dashboard from "./pages/Host/Dashboard.jsx";
+import Income from "./pages/Host/Income.jsx";
+import Reviews from "./pages/Host/Reviews.jsx";
+import HostVans from "./pages/Host/HostVans.jsx";
+import HostVanDetail from "./pages/Host/HostVanDetail.jsx";
+import HostVanInfo from "./pages/Host/HostVanInfo.jsx";
+import HostVanPricing from "./pages/Host/HostVanPricing.jsx";
+import HostVanPhotos from "./pages/Host/HostVanPhotos.jsx";
+import NotFound from "./pages/NotFound.jsx";
+import Layout from "./components/Layout.jsx";
+import HostLayout from "./components/HostLayout.jsx";
+import AuthRequired from "./components/AuthRequired.jsx";
+
+import "./server.js";
+
+function App() {
+  /**
+   * Challenge: Create the AuthRequired Layout Route to protect
+   * all the /host routes.
+   *
+   * For now, just use `const authenticated = false`
+   * to determine the authenticated status of the user, and
+   * either send them to the /login route, or render the Outlet
+   */
+
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Layout />}>
+          <Route index element={<Home />} />
+          <Route path="about" element={<About />} />
+          <Route path="vans" element={<Vans />} />
+          <Route path="vans/:id" element={<VanDetail />} />
+          <Route path="login" element={<Login />} />
+
+          <Route element={<AuthRequired />}>
+            <Route path="host" element={<HostLayout />}>
+              <Route index element={<Dashboard />} />
+              <Route path="income" element={<Income />} />
+              <Route path="reviews" element={<Reviews />} />
+              <Route path="vans" element={<HostVans />} />
+              <Route path="vans/:id" element={<HostVanDetail />}>
+                <Route index element={<HostVanInfo />} />
+                <Route path="pricing" element={<HostVanPricing />} />
+                <Route path="photos" element={<HostVanPhotos />} />
+              </Route>
+            </Route>
+          </Route>
+
+          <Route path="*" element={<NotFound />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
+  );
+}
+
+ReactDOM.createRoot(document.getElementById("root")).render(<App />);
